b313b1b7d4737243b8a309357f8eecb227df7bcf
Julien Palard <julien@palard.fr>
2018-07-03 15:22:38 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='poautofill',
-    version='0.1.0',
+    version='0.1.1',
     description="Autofill po files with automated translations.",
     long_description=readme,
     author="Julien Palard",
