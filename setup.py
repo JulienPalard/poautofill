04636362f2afdaed2f8965fe805a5a2426958fc6
Julien Palard <julien@palard.fr>
2018-09-23 23:31:09 +0200

    Once you go black, you never go back.

--- setup.py
+++ setup.py
@@ -3,38 +3,33 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='poautofill',
-    version='0.1.1',
+    name="poautofill",
+    version="0.1.1",
     description="Autofill po files with automated translations.",
     long_description=readme,
     author="Julien Palard",
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/poautofill',
-    packages=[
-        'poautofill',
-    ],
-    package_dir={'poautofill':
-                 'poautofill'},
-    entry_points={
-        'console_scripts': ['poautofill=poautofill.poautofill:fill_pos']
-    },
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/poautofill",
+    packages=["poautofill"],
+    package_dir={"poautofill": "poautofill"},
+    entry_points={"console_scripts": ["poautofill=poautofill.poautofill:fill_pos"]},
     include_package_data=True,
-    install_requires=['polib', 'requests', 'click'],
+    install_requires=["polib", "requests", "click"],
     license="MIT license",
     zip_safe=False,
-    keywords='poautofill',
+    keywords="poautofill",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
